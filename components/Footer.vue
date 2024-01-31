6ceee8cb05b04b92813ff8e2db0e6090b417d10d
Parth Sharma <parthsharma2880@gmail.com>
2024-02-01 01:26:59 +0530

    Added: Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+        <div class="row pb-3">
+            <div class="col-12 col-md-7 col-lg-6 col-sm-6 pt-3">
+                <div class="row no-gutters">
+                    <div class="col-auto">
+                        <!-- <img class="img-fluid" src="/wp-content/uploads/2020/10/footer-img.png" alt=""> -->
+                    </div>
+                    <div class="col">
+                        <div class="card-block px-2 text-left pl-2 pr-2 pl-lg-4 pr-lg-0">
+                            <p class="card-text w-lg-50 w-md-lg-25">Congratulations for traveling from start to the end of
+                                this page! We’ll add 10,000 steps to your fitness tracker!<br> 🙂</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-lg-2 col-sm-2 pt-3 text-left pl-4">
+                <ul>
+                    <li><a href="/about-us">About Us</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/project">Our Work</a></li>
+                    <li><a href="/clients">Clients</a></li>
+                    <li><a href="/our-team">Our Team</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2 col-lg-2 col-sm-2 pt-3 text-left">
+                <ul>
+                    <li><a href="/awards">Awards</a></li>
+                    <li><a href="/career/">Careers</a></li>
+                    <li><a href="/contact-us">Contact Us</a></li>
+                    <li><a href="/privacy-policy">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-12 col-lg-2 pt-3 text-right">
+                <ul
+                    class="social flex flex-row flex-wrap flex-lg-column justify-content-center items-center justify-content-md-end items-md-end">
+                    <li><a href="https://www.instagram.com/socialpanga/" target="_blank" rel="noopener">
+                        <!-- <img
+                                class="img-fluid"
+                                src="/wp-content/themes/socialpanga/assets/images/icons/instagram-icon.png"
+                                alt="Instagram"> -->
+                            </a>
+                        </li>
+                    <li><a href="https://twitter.com/socialpanga" target="_blank" rel="noopener">
+                        <!-- <img class="img-fluid"
+                                src="/wp-content/themes/socialpanga/assets/images/icons/twitter-icon.png" alt="Twitter"> -->
+                            </a>
+                    </li>
+                    <li><a href="https://www.facebook.com/socialpanga" target="_blank" rel="noopener">
+                        <!-- <img class="img-fluid"
+                                src="/wp-content/themes/socialpanga/assets/images/icons/facebook-icon.png"
+                            alt="Facebook"> -->
+                        </a>
+                    </li>
+                <li><a href="https://www.linkedin.com/company/socialpanga/" target="_blank" rel="noopener">
+                    <!-- <img
+                            loading="lazy" class="img-fluid"
+                            src="/wp-content/themes/socialpanga/assets/images/icons/linkedIn-icon.png" alt="LinkedIn"
+                            width="62" height="64"> -->
+                        </a>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</div></template>
