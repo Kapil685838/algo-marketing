24b01074db46607a12c5ef3dc558fbf99c60dead
Kapil Chaudhary <kc685838@gmail.com>
2024-02-02 23:44:24 +0530

    Updated: Active link of Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,7 @@
       <ul class="flex items-center gap-6">
         <li v-for="nav in root.nav.links" :key="nav.id">
           <NuxtLink :to="nav.to" class="flex flex-col items-center group">
-            <img :src="nav.img" class="w-12 -mb-1 invisible group-hover:visible" alt="" />
+            <img :src="nav.img" class="w-12 -mb-1 group-hover:visible" :class="route.path === nav.to ? '' : 'invisible'" alt="" />
             <span class="text-sm font-semibold">{{ nav.label.toUpperCase() }}</span>
           </NuxtLink>
         </li>
@@ -16,4 +16,6 @@
 
 <script setup>
 const root = useRootStore();
+
+const route = useRoute();
 </script>
