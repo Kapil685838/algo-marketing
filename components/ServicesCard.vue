999e877f26da1a29cd8e4861bcf0e93a34bf8265
Parth Sharma <parthsharma2880@gmail.com>
2024-02-04 20:00:42 +0530

    Added: Services page & services card

---
+++ components/ServicesCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-1/2 py-4 px-12">
+        <h3 v-html="props.title" class=" font-extrabold text-2xl"></h3>
+        <p class="text-xl">{{ props.description }}</p>
+    </div>
+
+    <div :class="`bg-[url('${props.img}')] hover:bg-[url('${props.gif}')]`" class="w-1/2 bg-cover bg-no-repeat bg-center">
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: '',
+    description: '',
+    img: '',
+    gif: ''
+})
+</script>
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        This is the services page.
+    <div class="container mx-auto">
+        <div class="flex justify-around pt-8">
+            <h1 class="w-1/2 text-6xl font-extrabold leading-normal px-4" v-html="root.service.title"></h1>
+            <div class="w-1/2 text-xl p-6">
+                <p>{{ root.service.description }}</p>
+            </div>
+        </div>
+
+        <div v-for="item in root.service.card" class="flex odd:flex-row-reverse mx-auto ">
+            <ServicesCard :key="item.id" :title="item.label" :description="item.description" :img="item.img" :gif="item.gif"/>
+        </div>
     </div>
 </template>
+
+<script setup>
+const root = useRootStore()
+</script>
