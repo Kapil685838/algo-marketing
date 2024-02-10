034402682a4b4e5aaa0547dda23a10c5bab00522
Kapil Chaudhary <kc685838@gmail.com>
2024-02-10 23:48:12 +0530

    Updated: Service card

--- components/ServicesCard.vue
+++ components/ServicesCard.vue
@@ -1,18 +1,16 @@
 <template>
+  <div v-for="service in root.services.cards" :key="service.id" class="flex items-center odd:flex-row-reverse">
     <div class="w-1/2 py-4 px-12">
-        <h3 v-html="props.title" class=" font-extrabold text-2xl"></h3>
-        <p class="text-xl">{{ props.description }}</p>
+      <h3 v-html="service.label" class="font-extrabold text-2xl"></h3>
+      <p class="text-xl">{{ service.description }}</p>
     </div>
 
-    <div :class="`bg-[url('${props.img}')] hover:bg-[url('${props.gif}')]`" class="w-1/2 bg-cover bg-no-repeat bg-center">
+    <div class="w-1/2 h-56 bg-cover bg-no-repeat bg-center" :class="`bg-[url('${service.img}')] border border-1`">
+        {{ service.img }}
     </div>
+  </div>
 </template>
 
 <script setup>
-const props = defineProps({
-    title: '',
-    description: '',
-    img: '',
-    gif: ''
-})
+const root = useRootStore();
 </script>
