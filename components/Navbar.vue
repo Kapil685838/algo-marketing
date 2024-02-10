6f58d7af20d53beb2443e188c5bfad4f651e5c7c
Kapil Chaudhary <kc685838@gmail.com>
2024-02-10 23:47:51 +0530

    Updated: Hover state of images

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,12 +5,8 @@
       <ul class="flex items-center space-x-8">
         <li v-for="nav in root.nav.links" :key="nav.id">
           <NuxtLink :to="nav.to" class="flex flex-col items-center group">
-            <img
-              :src="nav.img"
-              class="w-12 -mb-1 group-hover:visible"
-              :class="route.path === nav.to ? '' : 'invisible'"
-              alt=""
-            />
+            <img :src="nav.img" class="w-12 -mb-1 group-hover:visible" :class="route.path === nav.to ? '' : 'invisible'"
+              alt="" />
             <span class="text-sm font-medium">
               {{ nav.label.toUpperCase() }}
             </span>
