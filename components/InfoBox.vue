95a3e3130ed81c41722400cd44a4475040ca4363
Parth Sharma <parthsharma2880@gmail.com>
2024-02-25 17:01:13 +0530

    Updated: Responsiveness of Home-page

--- components/InfoBox.vue
+++ components/InfoBox.vue
@@ -2,11 +2,11 @@
     <div class="relative rounded-tl-[50px] bg-[#c32126] h-[500px] text-white">
         <div class="px-8 py-12">
             <p class="text-2xl font-black pt-">Looking to become the next big name everyone's talking about?</p>
-            <p class="text-xl mt-2 pb-14">Request a perposal.</p>
+            <p class="text-xl mt-2 pb-14 md:pb-24">Request a perposal.</p>
 
-            <div class="absolute -bottom-48 w-full">
+            <div class="w-full">
                 <img class="w-full" src="/images/infoxbox-ico1.png" alt="">
             </div>
-        </div>
+        </div>  
     </div>
 </template>
