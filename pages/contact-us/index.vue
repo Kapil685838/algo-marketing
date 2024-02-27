f7b83f07b8ff185e37daa7839c0af467637359ed
Parth Sharma <parthsharma2880@gmail.com>
2024-02-28 01:16:26 +0530

    Updated: Contact us page

--- pages/contact-us/index.vue
+++ pages/contact-us/index.vue
@@ -1,40 +1,54 @@
 <template>
-    <div class="container mx-auto">
-
-        <div class="maps absolute top-0 left-0 w-full mt-[90px]">
-            <iframe class="w-full"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d447989.905713128!2d76.76036936978494!3d28.692005556641707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d047309fff32f%3A0xfc5606ed1b5d46c3!2sDelhi!5e0!3m2!1sen!2sin!4v1708260203774!5m2!1sen!2sin"
-                width="600" height="450" style="border:0;" allowfullscreen=true loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade">
-                <div class="h-screen">
-                    hello
-                </div>
-            </iframe>
-        </div>
-        <div class="contact-form max-w-[41%] relative ml-24">
+    <div class="container mx-auto relative">
+        <div class="contact-form px-4 md:max-w-[41%] md:ml-24">
             <div class="pb-4 py-8 flex">
-                <div class="bg-primary p-12">
-                    <h2>Aapke Ek Ek Sawal
+                <div class="bg-primary p-4 md:p-12 z-10 my-32">
+                    <h2 class="text-2xl font-bold mb-6">Aapke Ek Ek Sawal <br />
                         Humaare Do Do Jawab
                     </h2>
-                    <p class="font-bold text-xs">Higa Digital Pvt. Ltd.</p>
+                    <p class="font-bold text-xs mb-6">Higa Digital Pvt. Ltd.</p>
 
-                    <p class="text-xs font-light">One of the leading digital marketing agency in Bangalore. Write to us
+                    <p class="text-xs leading-5 mb-6">One of the leading digital marketing agency in Bangalore. Write to us
                         about any doubts, We would be glad
                         to discuss your business targets and how we can achieve them through our digital marketing services
                         in
                         Bangalore, Delhi and Mumbai. We'll get back to you at the earliest.</p>
-                    <form class="flex flex-wrap gap-5">
-                        <input type="text" placeholder="Name" class="flex-[1_0_100%]">
-                        <input type="text" placeholder="Name" class="flex-[1_0_8rem]">
-                        <input type="text" placeholder="Name" class="flex-[1_0_8rem]">
-                        <textarea placeholder="Name" class="flex-[1_0_100%]" />
-                        <button type="button" class="w-[92px] h-[92px] rounded-full bg-[#e1e1e1]">
+                    <form class="flex flex-wrap gap-5 relative mb-20">
+                        <input type="text" placeholder="Name"
+                            class="placeholder-black flex-[1_0_100%] bg-transparent border-2 border-black">
+                        <div class="flex flex-col md:flex-row md:space-x-4 w-full">
+                            <input type="email" placeholder="Email"
+                                class="placeholder-black mb-5 md:mb-0 bg-transparent w-full md:w-auto border-2 border-black">
+                            <input type="tel" placeholder="Phone Number"
+                                class="placeholder-black bg-transparent w-full md:w-auto border-2 border-black">
+                        </div>
+                        <textarea placeholder="How can we help?" rows="12"
+                            class="placeholder-black flex-[1_0_100%] bg-transparent border-2 border-black"></textarea>
+                        <button type="button"
+                            class="w-[92px] h-[92px] absolute -bottom-16 right-1 rounded-full bg-[#e1e1e1] flex justify-center items-center">
                             <img src="/images/right-arrow-icon.png" alt="" class="w-[45%]">
                         </button>
                     </form>
+                    <div class="flex flex-col md:flex-row justify-center text-center">
+                        <div class="md:border-r border-black p-2 pr-4 ">
+                            <h2 class="font-bold">Bussiness Enquiries</h2>
+                            <a class="block">+91 99860 68516</a>
+                            <a class="font-semibold">hello@socialpanga.com</a>
+                        </div>
+                        <div class="p-2 pl-4">
+                            <h2 class="font-bold">Career Opportunities</h2>
+                            <a class="block">+91 99203 79306</a>
+                            <a class="font-semibold">coffee@socialpanga.com</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="maps absolute top-0 left-0 w-full h-full ">
+                    <iframe class="w-full h-full "
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d447989.905713128!2d76.76036936978494!3d28.692005556641707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d047309fff32f%3A0xfc5606ed1b5d46c3!2sDelhi!5e0!3m2!1sen!2sin!4v1708260203774!5m2!1sen!2sin"
+                        width="" height="" style="border:0;" allowfullscreen=true loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
                 </div>
             </div>
         </div>
-    </div>
-</template>
+</div></template>
