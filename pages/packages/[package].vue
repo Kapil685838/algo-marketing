ba55066e5470e746bcee7c4e329749ba1a664360
Kapil Chaudhary <kc685838@gmail.com>
2024-03-16 01:20:39 +0530

    Updated: Package.vue page

--- pages/packages/[package].vue
+++ pages/packages/[package].vue
@@ -1,10 +1,29 @@
 <template>
     <div v-if="data">
-        {{ data.description }}
+        <div v-if="data.hero" class="flex justify-between">
+            <div>
+                <div v-if="data.hero.tag">
+                    <div>Best</div>
+                    <p>{{ data.hero.tag }}</p>
+                </div>
+                <p v-if="data.hero.packageName">{{ data.hero.packageName }}</p>
+                <h1 v-if="data.hero.title">{{ data.hero.title }}</h1>
+                <div v-if="data.hero.description">
+                    <p v-for="item in data.hero.description" :key="item" v-html="item"></p>
+                </div>
+            </div>
+            <div>
+                <img class="md:max-w-xs" :src="`/images/${data.hero.img}`" alt="">
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-const { data } = await useFetch("/api/package");
+const route = useRoute();
+
+const { data } = await useFetch(`/api/packages/${route.params.package}`);
+
+console.log(data);
 
 </script>
