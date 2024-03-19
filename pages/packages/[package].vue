b93fdc87981029cfc5db5c868d5762a8822d9f04
Kapil Chaudhary <kc685838@gmail.com>
2024-03-20 00:53:07 +0530

    Added: OffPage SEO Services Section

--- pages/packages/[package].vue
+++ pages/packages/[package].vue
@@ -139,6 +139,49 @@
           </div>
         </div>
 
+        <!-- Off-Page SEO Services -->
+        <div v-if="data.mainContent.offPageServices" class="py-6">
+          <p class="text-3xl font-semibold my-6">
+            {{ data.mainContent.offPageServices.title }}
+          </p>
+          <p
+            v-for="item in data.mainContent.offPageServices.description"
+            :key="item"
+            v-html="item"
+            class="leading-loose *:has-[a]:text-red-500"
+          ></p>
+          <p class="leading-loose pt-4 pb-8">
+            {{ data.mainContent.offPageServices.services.title }}
+          </p>
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-12 gap-y-4">
+            <div
+              v-for="item in data.mainContent.offPageServices.services.list"
+              :key="item"
+              class="shrink max-h-min"
+            >
+              <div v-for="li in item" :key="li">
+                <div class="flex items-start gap-1.5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="size-5 mt-1.5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M10.5 15.25A.74.74 0 0 1 10 15l-3-3a.75.75 0 0 1 1-1l2.47 2.47L19 5a.75.75 0 0 1 1 1l-9 9a.74.74 0 0 1-.5.25"
+                    />
+                    <path
+                      fill="currentColor"
+                      d="M12 21a9 9 0 0 1-7.87-4.66a8.67 8.67 0 0 1-1.07-3.41a9 9 0 0 1 4.6-8.81a8.67 8.67 0 0 1 3.41-1.07a8.86 8.86 0 0 1 3.55.34a.75.75 0 1 1-.43 1.43a7.62 7.62 0 0 0-3-.28a7.43 7.43 0 0 0-2.84.89a7.5 7.5 0 0 0-2.2 1.84a7.42 7.42 0 0 0-1.64 5.51a7.43 7.43 0 0 0 .89 2.84a7.5 7.5 0 0 0 1.84 2.2a7.42 7.42 0 0 0 5.51 1.64a7.43 7.43 0 0 0 2.84-.89a7.5 7.5 0 0 0 2.2-1.84a7.42 7.42 0 0 0 1.64-5.51a.75.75 0 1 1 1.57-.15a9 9 0 0 1-4.61 8.81A8.67 8.67 0 0 1 12.93 21z"
+                    />
+                  </svg>
+                  <p class="leading-loose">{{ li }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <!-- FAQ's -->
         <div v-if="data.mainContent.faqs" class="py-6">
           <p class="text-3xl font-semibold text-center mb-6">
